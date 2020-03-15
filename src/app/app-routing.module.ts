6c2d8673a03e1c7bb094679dbbc2b63ce1350657
srujan <srujansurapaneni@gmail.com>
2020-03-14 22:40:03 -0400

    initial routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainNavigationComponent } from './components/main-navigation/main-navigation.component';
+import { MainHomeComponent } from './components/main-home/main-home.component';
+import { MainDashboardComponent } from './components/main-dashboard/main-dashboard.component';
+import { MainIndicatorsComponent } from './components/main-indicators/main-indicators.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainNavigationComponent},
+  { path: 'home', component: MainHomeComponent },
+  { path: 'dashboard', component: MainDashboardComponent },
+  { path: 'indicators', component: MainIndicatorsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
